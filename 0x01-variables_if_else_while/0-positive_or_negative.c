70ece664069fcf42fba9adc590483b08ce89917c
n-mbachia <ndmbachia@outlook.com>
2022-04-08 10:48:28 -0700

    execute random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
 
 /* program assigns random number each time when executed */
 
