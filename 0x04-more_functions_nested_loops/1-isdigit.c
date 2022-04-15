93e79d76071b0f68ddd9a7c912474992285d81b5
n-mbachia <ndmbachia@outlook.com>
2022-04-15 08:22:59 -0700

    checks digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _is digit determins if digit or not
+ * return: 1 if digital, 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if(c>=48 && c<=57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
