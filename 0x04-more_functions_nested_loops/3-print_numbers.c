6257736635edad5af980e8f174430c855331ce2c
n-mbachia <ndmbachia@outlook.com>
2022-04-15 08:59:28 -0700

    multiple two integers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints 0 to 9
+ * print_numbers - prints numbers 0 to 9
  * Return: numbers
  */
 
@@ -10,8 +10,8 @@ void print_numbers(void)
 	int i;
 
 	for (i = 48; i < 58; i++)
-	{ 
+	{
 		_putchar(i);
 	}
 	_putchar('\n');
-}			
+}
