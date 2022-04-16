1c23ebca2fb16e2e28b3aa7eeeef022cee8bb6ca
n-mbachia <ndmbachia@outlook.com>
2022-04-15 23:33:46 -0700

    straight line terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,16 +3,20 @@
 /**
  * print_line - print a line character
  * @n: length of line input
- * Return: nothing
+ * Return: Always 0. 
  */
 
 void print_line(int n)
-{
+{ 
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		_putchar(95);
+		for (i = 0; i < n; i++)
+			_putchar('_');
+
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
