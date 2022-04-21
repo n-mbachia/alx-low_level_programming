4be0df7e043f7621d3dfb76595e3d7bdfb487493
n-mbachia <ndmbachia@outlook.com>
2022-04-21 10:52:59 -0700

    prints a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string followed by new line
+ * @str: pointer to the string to print
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+
+		i++;
+	}
+	putchar('\n');
+}
