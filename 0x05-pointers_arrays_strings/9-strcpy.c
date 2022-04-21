0d3c98e78977ff61f57d13f65ca55cc7f9a3db2c
n-mbachia <ndmbachia@outlook.com>
2022-04-21 11:54:35 -0700

    copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,7 +4,7 @@
  * _strcpy - copies the string pointed to by src
  * @dest: char type string
  * @scr: char type string
- * description: copy string pointed to by pointer 'scr' to 
+ * description: copy string pointed to by pointer 'scr' to
  * the buffer pointed to by 'dest'
  * Return: void
  */
