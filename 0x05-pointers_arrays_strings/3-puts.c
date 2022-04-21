0adaed2dd4903be660ecfc13a9bde5e1c599bcd9
n-mbachia <ndmbachia@outlook.com>
2022-04-21 06:45:37 -0700

    prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by new line
+ * @str: pointer to the string to print
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int = 0;
+
+	while (str[i])
+	{
+		putchar(str[i]);
+		i++;
+	}
+
+	putchar('\n');
+}
