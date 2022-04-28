5ab6336c94dee02120838ac2460b13134993311a
n-mbachia <ndmbachia@outlook.com>
2022-04-28 19:21:02 +0000

    factrial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorails - returns the factorial of a given number. 
+ * @n: inputs number
+ * eturn: factorial of the number
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-11);
+	else if (n == 0)
+		return (1);
+	else retun (n * factorial(n-1)
+}
