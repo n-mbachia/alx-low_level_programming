987125c46708f1c535dbeb7f926dd6d814560611
n-mbachia <ndmbachia@outlook.com>
2022-04-28 19:15:58 +0000

    first commit

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,9 +1,9 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-int _putchar(chra);
-void _puts_rcursion(char *s);
-void _prints_rv_recursion(char *s);
+int _putchar(char);
+void _puts_recursion(char *s);
+void _prints_rev_recursion(char *s);
 int strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
