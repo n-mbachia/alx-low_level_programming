80dc8a016d306833837e670e9f30f0d46aed2075
n-mbachia <ndmbachia@outlook.com>
2022-04-28 19:13:31 +0000

    string length

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _prints_rev_recursion - prints reverse string.
+ * @s: string
+ * Return: no return
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+	
