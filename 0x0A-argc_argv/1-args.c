cce2f68ac028138e7adce38f07422e6c7f15bffe
n-mbachia <ndmbachia@outlook.com>
2022-05-02 19:49:33 +0000

    prints name and new line

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of rguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array tha contains the program command line arguments.
+ * Return: 0 - sucess.
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%i\n", arg -1);
+	return (0);
+}
