965e9a8bb0d5ba48d1a7499317a2498c916c6901
n-mbachia <ndmbachia@outlook.com>
2022-05-11 13:49:39 -0700

    executes function as given array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int i;
-
-	if (array && action)
-		for (i = 0; i < size; i++)
-			action(array[i]);
+	size_t i;
+	if array == NULL || action == NULL
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
