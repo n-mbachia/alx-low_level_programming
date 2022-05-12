0cb8ab2fc61ff455925413f02d42ca5b07ccb37f
n-mbachia <ndmbachia@outlook.com>
2022-05-12 06:18:26 +0000

    prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,16 @@
+#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name:input name
- * @f: function pointer
- *
- * Retunr: no return
+ * print_name - function that prints a name.
+ *@name: name
+ *@f: callback function
+ * Description: function that prints a name
  */
+
 void print_name(char *name, void (*f)(char *))
+
 {
-	if (!name || !f)
-		return;
+	if (name != NULL && f != NULL)
 	f(name);
 }
