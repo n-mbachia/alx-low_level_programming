755180723f51748e4c5e5eb28f9ba4389efe9478
n-mbachia <ndmbachia@outlook.com>
2022-05-12 06:41:30 +0000

    function that gives parameter on each element of array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,13 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned int  i;
+	size_t i;
 
-	if (!array || !action)
-		return;
-
+	if (array != NULL && action != NULL)
+		
 	for (i = 0; i < size; i++)
-		action(array[i]);
+	{
+		action(*array);
+		array++;
+	}
 }
