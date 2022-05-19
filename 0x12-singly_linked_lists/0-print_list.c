4163469e20b2c6dc2b03cf98be6834e75c1d3af9
n-mbachia <ndmbachia@outlook.com>
2022-05-19 12:54:56 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,21 +7,18 @@
   */
 size_t print_list(const list_t *h)
 {
-	int count;
+	size_t i =0;
 
-	count = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-			h = h->next;
-			count++;
-		}
-		printf("[%d] %s\n", h->len, h->str);
+
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		i++;
 		h = h->next;
-		count++;
 	}
 
-	return (count);
+	return (i);
 }
