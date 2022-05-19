b61a6e048bc9c12ba4ffebc0046470d460e0e0b6
n-mbachia <ndmbachia@outlook.com>
2022-05-19 13:58:54 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,12 @@
 #include "lists.h"
+
+
 /**
-  *print_list - prints all the elements of a list_t.
-  *@h: pointer to list.
-  *
-  *Return: the number of nodes.
-  */
+ * print_list - print counter all elements of `list_t` list
+ * @h: head of linked list
+ * Return: counter number of nodes
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t i = 0;
@@ -22,3 +24,4 @@ size_t print_list(const list_t *h)
 
 	return (i);
 }
+
