40570e230bb85571dc1d78445ea27603843b0fba
n-mbachia <ndmbachia@outlook.com>
2022-05-19 14:03:27 -0700

    Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void __attribute__((constructor)) print(void);
+void __attribute__((constructor)) print_first(void);
 
 /**
  * print_first - prints a string before the
  * main function is executed
  */
-`
+
 void print_first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
