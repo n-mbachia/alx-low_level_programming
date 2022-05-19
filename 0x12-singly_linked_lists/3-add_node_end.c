daaf0f5f09f8da4bde76d81eac79a4b0d8453c8c
n-mbachia <ndmbachia@outlook.com>
2022-05-19 13:54:46 -0700

    Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * _strlen - length of a string
- * @s: a pointer to an int that will be updated
+ *_strlen - length of a string
+ *@s: A pointer to an int that will be updated
  *
- * Return: void
+ *Return: void
  */
 
 int _strlen(const char *s)
@@ -12,44 +12,44 @@ int _strlen(const char *s)
 	int i;
 
 	i = 0;
-	while (s[i] != '\0') /* Count character of a string*/
+	while (s[i] != '\0') /*Count character of string*/
+	{
 		i++;
-	
+	}
+
 	return (i);
 }
 
 /**
- * add_node_end - add a new node at end of list
+ * add_node_end -  adds a new node at the end of a list
  * @head: pointer
- * @str: string
- * Return: address of new element
+ *@str:string
+ * Return:  address of the new element
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *last_node;
-	/*using malloc*/
+	list_t *new_node, *last_node = *head;
+	/*Using malloc*/
 	new_node = malloc(sizeof(list_t));
 
 
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strup(str); /*Assessing data*/
+	new_node->str = strdup(str); /*Assing data*/
 	new_node->len = _strlen(str);
 	new_node->next = NULL;
 
 	if (*head == NULL)
-		*head = new_node;
-	else
 	{
-		last_node = *head;
-
-		while (last_node->next != NULL)
-			last_node = last_node->next;
-		
-		last_node->next = new_node;
+		*head = new_node;
+		return (*head);
 	}
+	while (last_node->next != NULL)
+		last_node = last_node->next;
+
+	last_node->next = new_node;
 
 	return (*head);
 }
+
