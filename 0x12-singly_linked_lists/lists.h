3452eaac70c28bc3f4db6a66dd5d390c60070b33
n-mbachia <ndmbachia@outlook.com>
2022-05-19 14:10:31 -0700

    list header file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -27,5 +27,6 @@ list_t *add_node(list_t **head, const char *str);
 int _strlen(const char *);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void __attribute__((constructor)) print_first(void);
 
 #endif /*LIST_H*/
